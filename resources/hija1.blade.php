c8dc58d04685f44ecbdb6f4b9dacad4d98cad916
EstefaniHerrera <93635573+EstefaniHerrera@users.noreply.github.com>
2021-11-03 22:36:08 -0600

    vista proyecto laravel

---
+++ resources/hija1.blade.php
@@ -0,0 +1,12 @@
+@extends('plantillas.plantilla')
+
+@section('titulo', 'Hija 1')
+
+@section('contenido')
+    <h1>Seccion de Contenido en la Hija 1</h1>
+    <ol>
+        <li>Item 1</li>
+        <li>Item 2</li>
+        <li>Item 3</li>
+    </ol>
+@endsection
---
+++ resources/hija2.blade.php
@@ -0,0 +1,10 @@
+@extends('plantillas.plantilla')
+
+@section('titulo', 'Hija 2')
+
+@section('contenido')
+    <h1>Seccion de Contenido en la Hija 2</h1>
+    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Aliquid ratione ipsa doloribus expedita voluptas, error reprehenderit sint voluptates unde praesentium similique eos odio voluptatibus eius necessitatibus totam nesciunt quod sequi.</p>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat nostrum animi quam eius, nesciunt voluptate dolorum perferendis ipsum numquam doloremque, dolor similique natus vitae aut porro quo nemo quasi ex.</p>
+
+@endsection
